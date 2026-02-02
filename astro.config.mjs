6175fba7f409da033a4a2ecbdd8b3c8b34c6ab57
sathindu <sathindu.d.zoysa@gmail.com>
2026-02-02 21:53:00 +0530

    change the domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-domain.com', // Replace with your domain
+  site: 'https://user.github.io',
+  base: '/portfolio',
   devToolbar: {
     enabled: false
   },
