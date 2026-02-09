40e19aa6208a534fa44159d72096dcfc0f79dc0b
sathindu <sathindu.d.zoysa@gmail.com>
2026-02-09 11:01:22 +0530

    Redone the fix image bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sathindudezoysa.github.io',
-  base: '/',
+  base: "/",
   devToolbar: {
     enabled: false
   },
