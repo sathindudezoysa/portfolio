8065598dfb542c2654a9943a22fbf904ab5d3508
sathindu <sathindu.d.zoysa@gmail.com>
2026-03-29 15:15:16 +0530

    Revert "Delete astro.config.mjs"
    This reverts commit 7a48e32b65a8d47721bc5de0063c699c3c00603a.

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://sathindudezoysa.github.io',
+  base: '/',
+  devToolbar: {
+    enabled: false
+  },
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  vite: {
+    build: {
+      cssMinify: true,
+      minify: 'terser',
+    },
+  },
+});
